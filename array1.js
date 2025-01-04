0bd6b59cd166db29da48a0b4e832401279b7e51c
Vivek6034 <viveksingh6034@gmail.com>
2025-01-04 19:32:31 +0530

    new file

---
+++ array1.js
@@ -0,0 +1,22 @@
+// let ar = [2,4,'chiku',19.3,143,'vivek']
+// console.log(ar);
+
+// ar.push('aashu')
+// console.log(ar);
+
+// ar.pop()
+
+// ar.unshift(1)
+// console.log(ar);
+
+// ar.shift()
+// console.log(ar);
+
+
+let ar =[1,2,3,4,5,6]
+// let sl = ar.slice(1,4)
+// console.log(sl);
+// ar[0]='vksingh'
+// console.log(ar);
+let sl = ar.splice(1,3,'raj')
+console.log(ar);
