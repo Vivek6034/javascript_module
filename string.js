b962e335c3211ba044cbc715f85e1b1f661598b7
Vivek6034 <viveksingh6034@gmail.com>
2025-01-07 19:34:24 +0530

    new file

---
+++ string.js
@@ -0,0 +1,14 @@
+let str = "Hello"
+console.log(str[1]);
+
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+
+let ct = str.repeat(3)
+console.log(ct);
+
+
+
+
+
+
---
+++ template literall.js
@@ -0,0 +1,9 @@
+let tr =`trainer`
+
+let str1 = `hello
+my name is vivek
+how are you 
+i am technical ${tr}
+`
+
+console.log(str1);
