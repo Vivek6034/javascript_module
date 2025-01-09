dc02c97516d04c2055d0247cd12ac8aca184d11b
Vivek6034 <viveksingh6034@gmail.com>
2025-01-09 19:42:03 +0530

    math fun

---
+++ mathfun.js
@@ -0,0 +1,9 @@
+let num = 16.2545
+let st = Math.sqrt(num)
+let st2 = Math.trunc(num)
+
+console.log(st2);
+
+
+let num1 = Math.trunc(Math.random()*15+1)
+console.log(num1);
---
+++ random_num.js
@@ -0,0 +1,21 @@
+// function cng1() {
+//     document.getElementById("#stat1")
+//     let num1 = Math.trunc(Math.random()*15+1)
+//     document.write(num1).innerHtml.value
+
+// }
+
+const fun = ()=>{
+    let st1 = document.querySelector("#stat1") 
+    st1.innerHTML = Math.trunc(Math.random()*20+1)
+    
+    
+}
+const cng2 = () =>{
+    let st2 = document.querySelector("#stat2") 
+    st2.innerHTML = Math.trunc(Math.random()*20+1)
+}
+const cng3 = () =>{
+    let st3 = document.querySelector("#stat3") 
+    st3.innerHTML = Math.trunc(Math.random()*20+1)
+}
